3f1eea488345920f7cd60bae028bc0b8a5d538a1
harmolipi <nikob381@gmail.com>
2021-08-29 14:59:30 -0400

    Set up and associate User model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 50 }
   validates :body, presence: true, length: { maximum: 1000 }
   validates :user_id, presence: true
+
+  belongs_to :user
 end
