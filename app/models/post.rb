a718200407914f4a1eba108ec3ccf82c70362f20
harmolipi <nikob381@gmail.com>
2021-08-28 20:49:39 -0400

    Added Post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :body, presence: true, length: { maximum: 1000 }
+  validates :user_id, presence: true
 end
