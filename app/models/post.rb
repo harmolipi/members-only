b31d7f90e9cd3be0b2923e9793a7d77006a1ed28
harmolipi <nikob381@gmail.com>
2021-08-29 17:09:30 -0400

    Updated post feed display

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  validates :title, presence: true, length: { maximum: 50 }
+  validates :title, presence: true, length: { maximum: 40 }
   validates :body, presence: true, length: { maximum: 1000 }
   validates :user_id, presence: true
 
